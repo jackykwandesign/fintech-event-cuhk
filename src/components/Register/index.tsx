fbe44531d8af7926dbe94e1bb9d0963d558813b1
JKxAL <jackykwandesign@gmail.com>
2020-10-01 21:17:41 +0000

    try email login

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -30,7 +30,32 @@ const SignInScreen = (props:any) => {
     );
     return unregisterAuthObserver;
   },[1])
-
+  useEffect(()=>{
+    if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
+      console.log("signin with email")
+      // var email = window.localStorage.getItem('emailForSignIn');
+      // if (!email) {
+      //   // User opened the link on a different device. To prevent session fixation
+      //   // attacks, ask the user to provide the associated email again. For example:
+      //   email = window.prompt('Please provide your email for confirmation');
+      // }
+      // The client SDK will parse the code from the link for you.
+      firebase.auth().signInWithEmailLink("tszkitkwan88884@gmail.com", window.location.href)
+        .then(function(result) {
+          // Clear email from storage.
+          window.localStorage.removeItem('emailForSignIn');
+          // You can access the new user via result.user
+          // Additional user info profile not available via:
+          // result.additionalUserInfo.profile == null
+          // You can check if the user is new or existing:
+          // result.additionalUserInfo.isNewUser
+        })
+        .catch(function(error) {
+          // Some error occurred, you can inspect the code: error.code
+          // Common errors could be invalid email and invalid or expired OTPs.
+        });
+    }
+  },[1])
   function checkLoginOrSigninWithAuthResult(authResult: any, redirectUrl: any):boolean{
     console.log("signin success With Auth callback", authResult)
     // window.location.href = "/dashboard"
