b5d6807c9633cbfe0b42c171521862660f869dfe
JKxAL <jackykwandesign@gmail.com>
2020-10-10 18:09:25 +0000

    use heroku as backend

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,8 @@ module.exports = function(app) {
   app.use(
     '/api/',
     createProxyMiddleware({
-      // target: 'http://192.168.96.133:8080',
-      target: 'http://192.168.96.133:7000',
+      target: 'https://fintech-event-2020-be.herokuapp.com',
+      // target: 'http://192.168.96.133:7000',
       changeOrigin: true,
       pathRewrite: {
         '^/api': '',
