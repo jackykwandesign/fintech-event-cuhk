1c992c3b527e9319b2b5072c1a6a2f564c09d726
JKxAL <jackykwandesign@gmail.com>
2020-10-23 16:10:44 +0000

    responsive landing page

--- src/components/LandingPage/landingPage.tsx
+++ src/components/LandingPage/landingPage.tsx
@@ -2,6 +2,7 @@ import { Button, withStyles } from '@material-ui/core';
 import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { AppContext } from '../../contexts/firebaseContext/firebaseContext';
+import useWindowDimensions from '../WindowDimension/windowDimension';
 // import {ColorButton} from '../NavBar/navbar'
 import styles from './landingPage.module.css'
 
@@ -12,7 +13,10 @@ const LandingPage = () =>{
   const StyledButton = withStyles({
     root: {
       color:'white',
-      fontSize:"2rem",
+      fontSize:"1rem",
+      '@media (min-width:600px)': {
+        fontSize: '1.5rem',
+      },
       border:'3px solid white',
       transition:'all 0.3s ease-out',
       '&:hover': {
@@ -28,9 +32,16 @@ const LandingPage = () =>{
   })(Button);
   const {isSignin} = useContext(AppContext)
   const history = useHistory()
+  const { height, width } = useWindowDimensions();
+  console.log(`height: ${height}, width: ${width}`)
   return (
+    
     <div className={styles.hero_container}>
-      <img className = {styles.bgimg} src = "/images/backdrop.png"></img>
+      {/* <img className = {styles.bgimg} src = "/images/backdrop.png"></img> */}
+      {
+        width >= 1024 ? <img className = {styles.bgimg} src = "/images/backdrop.png"></img> : <img className = {styles.bgimg} src = "/images/backdrop-vertical.png"></img>
+      }
+      
       <div className={styles.hero_btns}>
        
        {
