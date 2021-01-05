a1044d4f00c51ab9e2af7085cf716dfa8edb145b
jackykwandesign <46065771+jackykwandesign@users.noreply.github.com>
2021-01-05 10:17:47 +0800

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -5,14 +5,14 @@ import 'firebase/auth'
 import 'firebase/firestore'
 import 'firebase/analytics'
 var firebaseConfig = {
-    apiKey: "AIzaSyB0cTpm3NnECEvUUQofnsG0I0SGpyAjKGo",
-    authDomain: "eventcuhk.firebaseapp.com",
-    databaseURL: "https://eventcuhk.firebaseio.com",
-    projectId: "eventcuhk",
-    storageBucket: "eventcuhk.appspot.com",
-    messagingSenderId: "488600771080",
-    appId: "1:488600771080:web:96bc27b06c99d8d5a14fbd",
-    measurementId: "G-PW7JP3FL4X"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
